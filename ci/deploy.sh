21517a722254ec1f099695332d59fb1189bccaa6
dasinlsb <399359237@qq.com>
2019-12-04 20:54:59 +0800

    Enable verbose output in deploy.sh

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -e
+
 ssh-keyscan -t $TRAVIS_SSH_KEY_TYPES -H $SERVER_IP 2>&1 | tee -a ${TRAVIS_HOME}/.ssh/known_hosts
 openssl aes-256-cbc -K $encrypted_4006a652c8d1_key -iv $encrypted_4006a652c8d1_iv -in ci/deploy_rsa.enc -out /tmp/deploy_rsa -d
 chmod 600 /tmp/deploy_rsa
